a59b88e1617c9ca18e5baa8dfdd2bb71bc86b082
abdelrahiim <eng_abdelrhiim@yahoo.com>
2023-10-02 20:01:28 +0300

    fc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@
 function App() {
     return (
         <div className="flex flex-col">
-            <h1>Hello For SSR React With Bun Which is Blazing Fast With ts</h1>
+            <h1 className="bg-blue-500">Hello For SSR React With Bun Which is Blazing Fast With ts</h1>
         </div>
     )
 }
